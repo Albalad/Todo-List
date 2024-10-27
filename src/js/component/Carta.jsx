c052b21b88df25138ca75ad62585b86d54d2bc6b
Alvaro <alvaro.albalad@gmail.com>
2024-10-27 09:03:37 +0000

    to do funcional

--- src/js/component/Carta.jsx
+++ src/js/component/Carta.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 //create your first component
-const Carta = () => {
+const Carta = ({task, eliminarTarea}) => {
     return (
-        <div>
+        <div className="col-md-4">
             <div className="card">
                 <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="card-link">X</a>
+                    <p className="card-text">{task}</p>
+                    <button className="btn btn-primary" onClick={()=>eliminarTarea()}>X</button>
                 </div>
             </div>
         </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,18 +5,51 @@ import Cabecera from "./Cabecera";
 
 //create your first component
 const Home = () => {
-	
-	const [task, setTask] = useState("")
 
+	//Creamos una variable vacía para que almacene los datos del input
+	let tarea ="";
+
+	// Creamos un array vacio para que almacene los datos que le llegan de la variable de tarea
+	const [task, setTask] = useState([]);
+
+	//Creamos la funcion para que el programa lea el valor que hay en el input y lo almacene en tarea
     const handleChange = (event) => {
-        setTask(event.target.value)
+		return tarea = event.target.value
     }
+
+	//Creamos la funcion para que añada un elemento nuevo al array task
+	const nuevaTarea = () =>{
+		//Validamos que si no hay informacion en la variable tarea no pueda añadir nada al array de task
+		if (tarea==""){
+			alert("introduce una tarea")
+		}else {
+			setTask([...task, tarea])
+		}
+	}
+
 
 	return (
 		<div>
-			<Cabecera input={handleChange}/>
-			<Carta/>
-			<p>{task}</p>
+			<Cabecera input={handleChange} nuevaTarea={nuevaTarea}/>
+			<div className="row">
+			{
+				//Mapeamos el array que tenemos en cada momento
+				task.map((element)=>{
+
+					//Creamos la funcion que elimina la carta seleccionada
+					const eliminarTarea = () =>{
+						setTask(task.filter((elemento)=>elemento!=element))
+					}
+
+					//Retornamos una carta por cada elemento del array
+					return(
+						<>
+							<Carta eliminarTarea={eliminarTarea} task={element}/>
+						</>
+					)
+				})
+			}
+			</div>
 		</div>
 	);
 };
